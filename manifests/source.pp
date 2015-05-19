13cef5880686e8ac4167fc0fabcc19965ec50b76
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-05-19 15:02:39 -0500

    Initial revision of choco_app::source type.

---
+++ manifests/source.pp
@@ -0,0 +1,20 @@
+# This defined type allows for the addition of custom sources
+# to the chocolatey.config.
+
+define choco_app::source (
+  $id = $name,
+  $url,
+  disabled = 'false',
+) inherits choco_app {
+
+  # This defined type simply wraps a concat fragment that will be
+  # added to the middle of the chocolatey.config file, between
+  # the <source> tags.
+  concat_fragment { "chocolatey source ${id}":
+    ensure => present,
+    target => 'chocolatey.config',
+    order => '50',
+    content => template('choco_app/chocolatey.config.source.erb'),
+  }
+
+}
