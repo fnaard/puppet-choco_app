207c9a84b60edc3300606bcaf4a47322deffa74b
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-05-19 13:53:39 -0500

    Add output redirection to the command

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class choco_app (
 
   # Use an exec of powershell to download and install Chocolatey.
   exec { 'install chocolatey':
-    command  => "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))",
+    command  => 'iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1")) >$null 2>&1',
     provider => 'powershell',
     creates  => 'C:\ProgramData\chocolatey\choco.exe',
   }
